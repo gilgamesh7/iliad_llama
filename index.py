58f3ecf3cce7a8c098a1d528c5d43cac1dd9a2a1
gilgamesh7 <felidaesrule@yahoo.com>
2023-04-06 20:52:16 +1200

    save index to file

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 import os
 import logging
+from pathlib import Path
 
 from llama_index import GPTSimpleVectorIndex, SimpleDirectoryReader
 
@@ -17,3 +18,10 @@ address = index.query("Where is the candidate's address?")
 
 logger.info(name)
 logger.info(address)
+
+index_file = Path()/'data'/'index.json'
+# save to disk
+index.save_to_disk(index_file)
+
+# load from disk
+index = GPTSimpleVectorIndex.load_from_disk(index_file)
