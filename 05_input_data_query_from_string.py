0b6afd6381ce9218146279d217cab389794ec812
gilgamesh7 <felidaesrule@yahoo.com>
2023-05-25 20:44:06 +1200

    Experiments with comparing responses

--- 05_input_data_query_from_string.py
+++ 05_input_data_query_from_string.py
@@ -2,7 +2,7 @@
 import os
 
 from llama_index import StringIterableReader, GPTTreeIndex
-
+import llama_index
 
 openai_api_key = os.environ.get('OPENAI_API_KEY')
 
@@ -17,5 +17,22 @@ index = GPTTreeIndex.from_documents(documents)
 
 response = index.query("Is this text comparing two or more things? Give a True or False answer")
 
-print(response)
 
+print(f" Response is : [{response}]")
+if response == True:
+    print("Response is True")
+else :
+    print("Cannot compare without  typecasting")
+
+
+print(f"Is response instance of type lama_index.response.schema.Response : {isinstance(response, llama_index.response.schema.Response)}")
+my_object = "True"
+my_object_as_Response = llama_index.response.schema.Response(my_object)
+print(f"Typecast {my_object} : {type(my_object_as_Response)}")
+print(f"Is my_object_as_Response instance of type lama_index.response.schema.Response : {isinstance(my_object_as_Response, llama_index.response.schema.Response)}")
+
+print(f"ID of my_object_as_Response : {my_object_as_Response} - {id(my_object_as_Response)}")
+print(f"ID of response : {response} -  {id(response)}")
+print(f"ID of my_object_as_Response : {my_object_as_Response} - {id(my_object_as_Response)}")
+print(f"Is  my_object_as_Response equal to response ? {my_object_as_Response == response}")
+print(f"Is  my_object_as_Response same as response ? {my_object_as_Response is response}")
